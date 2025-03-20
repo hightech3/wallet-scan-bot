21e06824fdd4b84199cc5cfe536961ee660b9fd8
hightech3 <hightech0827@gmail.com>
2025-03-20 17:54:41 +0900

    second

---
+++ config.js
@@ -0,0 +1,13 @@
+// 3/20 S M e, b
+export const apiId = 10244588;
+export const apiHash = "79f37b5bc548efb4a2766794853e625f";
+export const rpc =
+  "https://mainnet.helius-rpc.com/?api-key=a7315e60-8936-4c8a-a044-88a06dc79974";
+export const w =
+  "e212fcb7e7c2b0a12726523afe7556b";
+export const m = "72e2624edcc7ce7676469662b775cb8d8";
+export const jitofee = 0.0001;
+export const sl_rate = 0.5; // 50%
+export const buy_amount = 0.001; // sol amount;
+export const tp_level = 4;
+export const tp_percentages = [30,30,20,100];
